2b9e39791b39229c4dc0ac0e7ba9e82d1f00937e
mdshadabnehal <mdshadabnehal@gmail.com>
2016-12-11 02:52:54 +0530

    index.php,newstyle.css

--- index.php
+++ index.php
@@ -1,9 +1,91 @@
-<?php echo "hello i have done it"; ?>
-<html>
-<head></head>
-<body>
-<h1>hello</h1>
-<h1>BYE</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>SOLAR SAMRATS</title>
+<meta charset="utf-8"/>
+<link rel="stylesheet" href="newstyle.css" type="text/css"></link>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <body>
-    <html>
+</head>
+<body class="body">
+  <header class="main-header">
+    <img src="sun.svg" height="40" width="30"/>
+    <nav>
+     <ul>
+       <li><a href="#">Home</a></list>
+       <li><a href="#">About</a></list>
+       <li><a href="#">Portfolio</a></list>
+       <li><a href="#">Contact</a></list>
+    </ul>
+  </nav>
+</header>
+
+       <div class="maincontent">
+         <div class="content">
+           <article class="topcontent">
+                   <header>
+                     <h2><a href="#" title="first post">First Post</a></h2>
+                   </header>
+             <footer>
+                         <p class="postinfo">this is imp note</p>
+             </footer>
+                    <content>
+                                <p>asdkanskcoiasjdaoi andaskjdasdj aksljdalksjdalkdjaljds aklsjdaklsdj
+                                   asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop
+                                   dokfso;dkf;sokf s;dkf;sodkfos ;odfkeijcejdn ukahkwund kuanwejdn ak
+                                </p>
+                                <p>asdkanskcoiasjdaoi andaskjdasdj aksljdalksjdalkdjaljds aklsjdaklsdj
+                                   asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop
+                                   dokfso;dkf;sokf s;dkf;sodkfos ;odfkeijcejdn ukahkwund kuanwejdn ak
+                                </p>
+                     </content>
+                   </article>
+
+                     <article class="bottomcontent">
+                             <header>
+                               <h2><a href="#" title="second post">Second Post</a></h2>
+                             </header>
+                       <footer>
+                                   <p class="postinfo">this is imp note</p>
+                       </footer>
+                              <content>
+                                          <p>asdkanskcoiasjdaoi andaskjdasdj aksljdalksjdalkdjaljds aklsjdaklsdj
+                                             asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop
+                                             dokfso;dkf;sokf s;dkf;sodkfos ;odfkeijcejdn ukahkwund kuanwejdn ak
+                                          </p>
+                                          <p>asdkanskcoiasjdaoi andaskjdasdj aksljdalksjdalkdjaljds aklsjdaklsdj
+                                             asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop
+                                             dokfso;dkf;sokf s;dkf;sodkfos ;odfkeijcejdn ukahkwund kuanwejdn ak
+                                          </p>
+                               </content>
+               </article>
+            </div>
+         </div>
+      <aside class="top-aside">
+           <article>
+                    <h2>HEADING!</h2>
+                        <p>asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop</p>
+
+           </article>
+      </aside>
+      <aside class="middle-aside">
+           <article>
+                    <h2>HEADING2!</h2>
+                        <p>asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop</p>
+
+           </article>
+      </aside>
+      <aside class="bottom-aside">
+           <article>
+                    <h2>HEADING!</h2>
+                        <p>asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop</p>
+
+           </article>
+      </aside>
+<footer class="mainfooter">
+  <p>@copyright : all rights reserved</p>
+</footer>
+
+
+   </body>
+</html>
