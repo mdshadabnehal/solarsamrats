357b01bf69d73e091c8273bdfecbd54c84a46ec4
mdshadabnehal <mdshadabnehal@gmail.com>
2016-12-17 22:43:35 +0530

    new files 17-12-2016

--- index.php
+++ index.php
@@ -1,91 +1,347 @@
-<!DOCTYPE html>
-<html lang="en">
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
 <head>
 <title>SOLAR SAMRATS</title>
-<meta charset="utf-8"/>
-<link rel="stylesheet" href="newstyle.css" type="text/css"></link>
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Mechanize  Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--Google Fonts-->
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/paccordion.js"></script>
+<!--google fonts-->
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
 
 </head>
-<body class="body">
-  <header class="main-header">
-    <img src="sun.svg" height="40" width="30"/>
-    <nav>
-     <ul>
-       <li><a href="#">Home</a></list>
-       <li><a href="#">About</a></list>
-       <li><a href="#">Portfolio</a></list>
-       <li><a href="#">Contact</a></list>
-    </ul>
-  </nav>
-</header>
+<body>
+<!--header start here-->
+<div class="header">
+	<div class="container">
+	  <div class="header-main">
+	     <div class="logo">
+		    <h1><a href="index.html">SOLAR SAMRAT</a></h1>
+		 </div>
+		 <div class="top-nav">
+		 	 <span class="menu"> <img src="images/icon.png"></span>
+		   	<nav class="cl-effect-13" id="cl-effect-13">
+					<ul class="res">
+						<li><a class="active" href="index.html">Home</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="gallery.html">Gallery</a></li>
+						<li><a href="#contact" class="scroll">Contact</a></li>
+					</ul>
+				</nav>
+				<!-- script-for-menu -->
+								 <script>
+								   $( "span.menu" ).click(function() {
+									 $( "ul.res" ).slideToggle( 300, function() {
+									 // Animation complete.
+									  });
+									 });
+								</script>
+				<!-- /script-for-menu -->
 
-       <div class="maincontent">
-         <div class="content">
-           <article class="topcontent">
-                   <header>
-                     <h2><a href="#" title="first post">First Post</a></h2>
-                   </header>
-             <footer>
-                         <p class="postinfo">this is imp note</p>
-             </footer>
-                    <content>
-                                <p>asdkanskcoiasjdaoi andaskjdasdj aksljdalksjdalkdjaljds aklsjdaklsdj
-                                   asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop
-                                   dokfso;dkf;sokf s;dkf;sodkfos ;odfkeijcejdn ukahkwund kuanwejdn ak
-                                </p>
-                                <p>asdkanskcoiasjdaoi andaskjdasdj aksljdalksjdalkdjaljds aklsjdaklsdj
-                                   asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop
-                                   dokfso;dkf;sokf s;dkf;sodkfos ;odfkeijcejdn ukahkwund kuanwejdn ak
-                                </p>
-                     </content>
-                   </article>
+		 </div>
+	     <div class="clearfix"> </div>
+	  </div>
+	</div>
+</div>
+<!--header end here-->
+<!--banner start here-->
+<div class="banner">
+  <div  class="container">
+    <div class="banner-main">
+    	<section class="slider">
+		 <div class="flexslider">
+			<ul class="slides">
+			  <li>
+			  	<div class="bann-left">
+				    <h3>WE OFFER BEST SERVICES</h3>
+				    <p>There is no way we are letting our costumers down.We are ready to face any challenges.For us our costumers are the first priority</p>
+				    <div class="bann-btn">
+				    	<a href="single.html">Read More</a>
+				    </div>
+			    </div>
+			  </li>
+			  <li>
+			  	<div class="bann-left">
+				    <h3>WE OFFER BEST SERVICES</h3>
+				    <p>There is no way we are letting our costumers down.We are ready to face any challenges.</p>
+				    <div class="bann-btn">
+				    	<a href="single.html">Read More</a>
+				    </div>
+			    </div>
+			  </li>
+			  <li>
+			  	<div class="bann-left">
+				    <h3>There is no way we are letting our costumers down.We are ready to face any challenges.For</h3>
+				    <p>There is no way we are letting our costumers down.We are ready to face any challenges.For.</p>
+				    <div class="bann-btn">
+				    	<a href="single.html">Read More</a>
+				    </div>
+			    </div>
+			  </li>
+		    </ul>
+		 </div>
+		 <div class="clearfix"> </div>
+      </section>
+	</div>
+  </div>
+</div>
+<!--banner end here-->
+<!-- FlexSlider -->
+				  <script defer src="js/jquery.flexslider.js"></script>
+				  <script type="text/javascript">
+					$(function(){
+					});
+					$(window).load(function(){
+					  $('.flexslider').flexslider({
+						animation: "slide",
+						start: function(slider){
+						  $('body').removeClass('loading');
+						}
+					  });
+					});
+				  </script>
+<!-- FlexSlider -->
+<!--we are start here-->
+<div class="we-are">
+	<div class="container">
+	  <div class="we-are-main">
+	     <div class="col-md-6 we-are-left">
+	     	<h2>How we do it </h2>
+	     	<p>There is no way we are letting our costumers down.We are ready to face any challenges.For </p>
+	     	<a href="single.html">Read more</a>
+		 </div>
+		 <div class="col-md-6 we-are-rights">
+		      <img src="solar-image-1.jpg" alt="" class="img-responsive">
+		 </div>
+	     <div class="clearfix"> </div>
 
-                     <article class="bottomcontent">
-                             <header>
-                               <h2><a href="#" title="second post">Second Post</a></h2>
-                             </header>
-                       <footer>
-                                   <p class="postinfo">this is imp note</p>
-                       </footer>
-                              <content>
-                                          <p>asdkanskcoiasjdaoi andaskjdasdj aksljdalksjdalkdjaljds aklsjdaklsdj
-                                             asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop
-                                             dokfso;dkf;sokf s;dkf;sodkfos ;odfkeijcejdn ukahkwund kuanwejdn ak
-                                          </p>
-                                          <p>asdkanskcoiasjdaoi andaskjdasdj aksljdalksjdalkdjaljds aklsjdaklsdj
-                                             asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop
-                                             dokfso;dkf;sokf s;dkf;sodkfos ;odfkeijcejdn ukahkwund kuanwejdn ak
-                                          </p>
-                               </content>
-               </article>
-            </div>
-         </div>
-      <aside class="top-aside">
-           <article>
-                    <h2>HEADING!</h2>
-                        <p>asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop</p>
+	  </div>
+	</div>
+</div>
+<!--we are end here-->
+<!--special start here-->
+<div class="special">
+	<div class="container">
+		<div class="special-main">
+			<div class="col-md-5 special-left">
+				<h3>Specialty</h3>
+				<p>We deal in all kinds of solar related products with strict quality compliance</p>
 
-           </article>
-      </aside>
-      <aside class="middle-aside">
-           <article>
-                    <h2>HEADING2!</h2>
-                        <p>asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop</p>
+				<p>Our products are the best meeting with good pricing and sound technical requirements.</p>
 
-           </article>
-      </aside>
-      <aside class="bottom-aside">
-           <article>
-                    <h2>HEADING!</h2>
-                        <p>asdadjao lskdjfsdjflkdjflskdfjk sldkjflskdjjflskdjflsdjk lkdfjslsop</p>
+				<p>Our main motto is to make our enviornment green and pollution free by making our clients happy.</p>
+			</div>
+			<div class="col-md-7 special-right">
+				<div class="special-grid-main">
+					<div class="col-md-4 special-grid">
+						<img src="images/s1.png" alt="">
+						<h3>Lead</h3>
+					</div>
 
-           </article>
-      </aside>
-<footer class="mainfooter">
-  <p>@copyright : all rights reserved</p>
-</footer>
+					<div class="col-md-4 special-grid">
+						<img src="images/s3.png" alt="">
+						<h3>Shop</h3>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="special-grid-main">
+					<div class="col-md-4 special-grid">
+						<img src="images/s4.png" alt="">
+						<h3>Worker</h3>
+					</div>
 
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--special end here-->
+<!--mechanize start here-->
+<div class="mechanize">
+	<div class="container">
+		<div class="mechanize-main">
+			 <div class="col-md-4 mechani-left">
+			 	<h3>GO SOLAR,GO GREEN</h3>
+			 	<h4>Solar is the best energy source.</h4>
+			 	<p>But I must explain to you how we can make use of this energy.We can use variety of solar products like solar inverters,solar panels,solar geysers,solar heaters,solar chargers and the list goes on.... .</p>
+			 </div>
+			 <div class="col-md-8 mechani-right">
+			 	 <div class="mechanize-grid">
+				 	<div class="col-md-4 mechanical-bott-left view view-sixth">
+				 		   <img src="images/w3.jpg" alt="" class="img-responsive">
+				 		<div class="mask">
+				 			<h3>Solar Management</h3>
+				 		</div>
+				 	</div>
+				 	<div class="col-md-8 mechanical-bott-rit">
+				 		<h5>Increasing Potential</h5>
+				 		<p>By making these products we feel free and relieved as our products serve a great cause.</p>
+				 		<a href="single.html">Read More</a>
+				 	</div>
+				 	<div class="clearfix"> </div>
+			    </div>
+			    <div class="mechanize-grid">
+				 	<div class="col-md-4 mechanical-bott-left  view view-sixth">
 
-   </body>
+				 		  <img src="images/w4.jpg" alt="" class="img-responsive">
+				 		<div class="mask">
+				 			<h3>Just Get Solar</h3>
+				 		</div>
+				 	</div>
+				 	<div class="col-md-8 mechanical-bott-rit">
+				 		<h5>Risk Management</h5>
+				 		<p>Solar energy management decreases all chanses of life risks which poses a threat in case of thermal or any other generated power.</p>
+				 		<a href="single.html">Read More</a>
+				 	</div>
+				 	<div class="clearfix"> </div>
+			    </div>
+			 </div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+<!--mechanize end here-->
+<!--Industry grid start here-->
+
+						</div>
+					</div>
+					<script>
+							jQuery(document).ready(function() {
+								function close_accordion_section() {
+									jQuery('.accordion .accordion-section-title').removeClass('active');
+									jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+								}
+
+								jQuery('.accordion-section-title').click(function(e) {
+									// Grab current anchor value
+									var currentAttrValue = jQuery(this).attr('href');
+
+									if(jQuery(e.target).is('.active')) {
+										close_accordion_section();
+									}else {
+										close_accordion_section();
+
+										// Add active class to section title
+										jQuery(this).addClass('active');
+										// Open up the hidden content panel
+										jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+									}
+
+									e.preventDefault();
+								});
+							});
+				</script>
+				</div>
+				<div class="col-md-7 industrial-right">
+					<h3>Recent News</h3>
+					<div class="news-grid">
+						<div class="new-lft-main">
+							<div class="news-left">
+								<h4>25</h4>
+								<h5>July</h5>
+							</div>
+						</div>
+						<div class="news-right">
+							<h6>Lorem ipsum dolor sit amet</h6>
+							<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
+						</div>
+					  <div class="clearfix"> </div>
+					</div>
+					<div class="news-grid">
+						<div class="new-lft-main">
+							<div class="news-left">
+								<h4>30</h4>
+								<h5>July</h5>
+							</div>
+						</div>
+						<div class="news-right">
+							<h6>Lorem ipsum dolor sit amet</h6>
+							<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
+						</div>
+					  <div class="clearfix"> </div>
+					</div>
+				</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+<!--Industry grid end here-->
+<!--get in touch start here-->
+<div class="touch-wthree" id="contact">
+	<div class="container">
+		<div class="touch-wthree-main w3ls">
+			<div class="touch-wthree-top">
+				 <h3>Touch With Us</h3>
+				 <p>Feel free to contact us anytime anywhere</p>
+			</div>
+			<div class="touch-wthree-bottom">
+				<div class="col-md-5 touch-wthree-left">
+					<h4>Contact Address</h4>
+					<ul>
+				    	<li><span class="glyphicon glyphicon-map-marker" > </span>Sector 10 market shop no </li>
+				    	<li><span class="glyphicon glyphicon-map-marker"> </span>Faridabad(Haryana),India</li>
+			        </ul>
+			        <h5>Mobile</h5>
+			        <ul>
+				    	<li><span class="glyphicon glyphicon-earphone"> </span>8867791354</li>
+				    	<li><span class="glyphicon glyphicon-phone" > </span>9818061929</li>
+
+			        </ul>
+			        <h6>Email</h6>
+			        <ul>
+			           <li><span class="glyphicon glyphicon-envelope" > mdshadabnehal@gmail.com</span></li>
+			        </ul>
+				</div>
+				<div class="col-md-7 touch-wthree-right">
+					<form action="#" method="post">
+						<input type="text" placeholder="Your Name" name="Your Name" required="">
+						<input type="text" class="no-mar" placeholder="Phone" name="Phone">
+						<input type="text" placeholder="Mail" name="Mail">
+						<input type="text" class="no-mar" placeholder="Website" name="Website">
+						<textarea name="Message" placeholder="Messages"></textarea>
+						<input type="submit" value="Submit">
+					</form>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--get in touch end here-->
+<!--map start here-->
+<div class="copy-right w3l">
+	<div class="container">
+		 <div class="copy-rights-main">
+		 		  <p>© 2016 Solar Samrets. All Rights Reserved | Design by Shadab  </p>
+    	 </div>
+    </div>
+</div>
+<!--copy right end here-->
+</body>
 </html>
